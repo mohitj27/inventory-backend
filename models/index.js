3d7a289295fef1234cfa1dd6c8715dec7012956b
Sagar Tambe <154309797+sagartambe2023@users.noreply.github.com>
2024-05-18 23:03:10 +0530

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 
 const mongoose = require("mongoose");
 
-const uri = "mongodb+srv://contacttosagartambe:sagar@123@inventory-db.dysiuzp.mongodb.net/?retryWrites=true&w=majority&appName=Inventory-db";
+const uri = "mongodb://localhost:27017/InventoryManagement";
 
 mongoose.connect(uri, {
   useNewUrlParser: true,
