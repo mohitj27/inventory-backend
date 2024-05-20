c81fe686bf73f1d93d37a24b015c52c6ec74ebac
Sagar Tambe <154309797+sagartambe2023@users.noreply.github.com>
2024-05-20 14:01:26 +0530

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 
 const mongoose = require("mongoose");
 
-const uri = "mongodb+srv://adminhamza:adminhamza123&@cluster0.pzcviot.mongodb.net/InventoryManagementApp?retryWrites=true&w=majority";
+const uri = "mongodb://localhost:27017/InventoryManagement";
 
 mongoose.connect(uri, {
   useNewUrlParser: true,
